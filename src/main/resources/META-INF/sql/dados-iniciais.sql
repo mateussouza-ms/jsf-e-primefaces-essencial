f6cd5a95cd765db882c59bcbfd5ed3c4920849e4
Mateus Souza <mateus_souza_teles@hotmail.com>
2022-06-14 23:34:45 -0300

    Create a git project to upload to github

---
+++ src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -0,0 +1,15 @@
+insert into ramo_atividade (descricao) values ('Distribuição de alimentos');
+insert into ramo_atividade (descricao) values ('Telecomunicações');
+insert into ramo_atividade (descricao) values ('Vestuário');
+insert into ramo_atividade (descricao) values ('Lavanderia');
+insert into ramo_atividade (descricao) values ('Gráfica');
+insert into ramo_atividade (descricao) values ('Mecânica');
+insert into ramo_atividade (descricao) values ('Turismo');
+insert into ramo_atividade (descricao) values ('Saúde');
+insert into ramo_atividade (descricao) values ('Educação');
+insert into ramo_atividade (descricao) values ('Lazer');
+
+insert into empresa (cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values ('70.311.193/0001-87', 'Mercado do João', 'João Mercado e Distribuidor de Alimentos Ltda', 'LTDA', '2009-03-02', 1);
+insert into empresa (cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values ('52.822.994/0001-25', 'Fale Mais', 'Fale Mais Telecom S.A.', 'SA', '1997-12-10', 2);
+insert into empresa (cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values ('41.952.519/0001-57', 'Maria de Souza da Silva', 'Maria de Souza da Silva', 'MEI', '2014-10-15', 3);
+insert into empresa (cnpj, nome_fantasia, razao_social, tipo, data_fundacao, ramo_atividade_id) values ('16.134.777/0001-89', 'Gomes Inovação', 'José Fernando Gomes EIRELI ME', 'EIRELI', '2009-03-02', 4);
